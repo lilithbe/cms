bd10b62b314a28f4d96cf15ee34d7d3c3f75e8df
LILITH-SERVER\server <lilithbe.dev@gmail.com>
2022-03-16 12:53:11 +0900

    update

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,3 @@
-// const {
-//   PHASE_DEVELOPMENT_SERVER,PHASE_PRODUCTION_BUILD
-// } = require('next/constants')
-  //yarn dev
-  // const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  //yarn build
-  // const isProd = phase ===PHASE_PRODUCTION_BUILD && prosess.env.STAGING !=='1'
-  //yarn build
-  // const isStaging = phase ===PHASE_PRODUCTION_BUILD && prosess.env.STAGING ==='1'
-
-//   const env = {
-//     TITLE:(()=>{
-//       if(isDev) return 'Title Dev'
-//       if(isProd) return 'Title Prod'
-//       if(isStaging) return 'Title Stg'
-//     })()
-//   }
-
 const withTM = require('next-transpile-modules')([
   'suneditor-react',
   'suneditor',
@@ -39,7 +21,7 @@ module.exports =withTM(
     distDir: 'build',
     //
     images: {
-      domains: ['www.lilith.co.kr','k.kakaocdn.net','lh3.googleusercontent.com','storagelilith.blob.core.windows.net'],
+      domains: [process.env.NEXT_PUBLIC_DOMAIN,'k.kakaocdn.net','lh3.googleusercontent.com',`${process.env.NEXT_PUBLIC_AZUER_STORAGE}.blob.core.windows.net`],
     },
     
   
